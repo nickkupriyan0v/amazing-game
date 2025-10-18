a62323af9d03784746dd447d3d78b7a3e5eeb5ab
vkk58 <twicvk@gmail.com>
2025-10-18 11:03:59 +0300

    Add request

--- packages/client/src/pages/loginPage/index.tsx
+++ packages/client/src/pages/loginPage/index.tsx
@@ -1,17 +1,69 @@
+import { Button, Container, Field, Input, Stack } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router'
 import { ROUTES } from '../../constants/routes'
+import { LoginPageRequest } from './request'
 
 interface FormValues {
   login: string
   password: string
 }
+
 const LoginPage = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
+    setError,
   } = useForm<FormValues>()
 
-  const onSubmit = handleSubmit((data: FormValues) => console.log(data))
+  const onSubmit = handleSubmit((data: FormValues) => {
+    const loginRequest = LoginPageRequest.getInstance()
+
+    loginRequest.signIn(data).then(result => {
+      if (result.success) {
+        navigate(ROUTES.mainPage)
+      } else {
+        setError('password', {
+          type: 'server',
+          message: result.error,
+        })
+      }
+    })
+  })
+  const navigate = useNavigate()
+
+  return (
+    <Container
+      maxW="container.md"
+      display="flex"
+      alignItems="center"
+      justifyContent="center">
+      <form onSubmit={onSubmit}>
+        <Stack gap="4" align="flex-start" maxW="sm">
+          <Field.Root invalid={!!errors.login}>
+            {/* @ts-ignore */}
+            <Field.Label>Логин</Field.Label>
+            <Input {...register('login')} />
+            {/* @ts-ignore */}
+            <Field.ErrorText>{errors.login?.message}</Field.ErrorText>
+          </Field.Root>
+
+          <Field.Root invalid={!!errors.password}>
+            {/* @ts-ignore */}
+            <Field.Label>Пароль</Field.Label>
+            <Input {...register('password')} />
+            {/* @ts-ignore */}
+            <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+          </Field.Root>
+
+          <Button type="submit" loading={isSubmitting} loadingText="Вход...">
+            Войти
+          </Button>
+        </Stack>
+      </form>
+    </Container>
+  )
 }
+
+export default LoginPage
