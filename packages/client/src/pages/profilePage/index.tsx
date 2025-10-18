e674d5f5d97e8a18b8514cc60b4c5187ce208c5c
Aoi <naztya2001@bk.ru>
2025-10-18 12:54:19 +0300

    init

--- packages/client/src/pages/profilePage/index.tsx
+++ packages/client/src/pages/profilePage/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { Container, Image, Text, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>
+  // avatar, profile info (display name, login, password), change pass modal, change avatar modal
+  // use effect for avatar change, useEffect for pass change
+  const [imgSrc, setSrc] = useState()
+  const [login, setLogin] = useState()
+  const displayName = login
+
+  const changeAvatar = () => {
+    console.log('changeAvatar')
+  }
+  const changePassword = () => {
+    console.log('change pass')
+  }
+
+  return (
+    <Container
+      maxW="container.md"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center">
+      <Container display="flex" alignItems="center" justifyContent="center">
+        <Image src={imgSrc} />
+        <Button onClick={changeAvatar}></Button>
+      </Container>
+      <Text>{displayName}</Text>
+      <Text>{login}</Text>
+      <Button onClick={changePassword}></Button>
+    </Container>
+  )
 }
 
 export default ProfilePage
