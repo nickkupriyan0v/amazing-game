83ee49bcd6bc797eaacb12b491f8199733b82325
Nurtazenov <a.nurtazenov@yandex.ru>
2025-10-20 02:05:09 +0300

    getStart

---
+++ packages/client/src/pages/getStartPage/getStartPage.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import './getStartPage.css'
+export const GetStartPage: React.FC<{ getStart: () => void }> = ({
+  getStart,
+}) => {
+  const [timer, setTimer] = useState<number | null>(null)
+
+  useEffect(() => {
+    if (timer === null || timer === 0) return
+    const countdown = setTimeout(() => setTimer(timer - 1), 1000)
+    if (timer === 1) {
+      getStart()
+    }
+    return () => clearTimeout(countdown)
+  }, [timer])
+
+  return (
+    <div className="startScreen">
+      {timer === null ? (
+        <div className="start-content">
+          <h1>Игра на память</h1>
+          <p>Найди все пары одинаковых карточек как можно быстрее!</p>
+          <button className="start-button" onClick={() => setTimer(3)}>
+            Начать игру
+          </button>
+        </div>
+      ) : (
+        <div className="count-down">
+          <div className="loader"></div>
+          <h2 className="game-countdown">Игра начнется через {timer}...</h2>
+        </div>
+      )}
+    </div>
+  )
+}
