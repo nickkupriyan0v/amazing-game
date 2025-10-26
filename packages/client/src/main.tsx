7cfe82197921dfd36e66b46fe77c1af2c72fe4e8
Aoi <naztya2001@bk.ru>
2025-10-26 19:09:27 +0300

    init: serviceWorker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,13 +4,22 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import useServiceWorker from './hooks/useServiceWorker/useServiceWorker'
+
+const Root = () => {
+  useServiceWorker()
+
+  return (
+    <React.StrictMode>
+      <ChakraProvider value={defaultSystem}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </React.StrictMode>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>
+  <Root />
 )
