966a878a771226bd12971a88e0b6ee055499a95a
sbsabdulaziz1402 <sbsabdulaziz1402@gmail.com>
2025-11-18 08:52:27 +0500

    fixed memory leak

--- packages/client/src/hooks/useGameLogic/useGameLogic.ts
+++ packages/client/src/hooks/useGameLogic/useGameLogic.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 import { Card } from '../../types/card'
 
 export const useGameLogic = (cards: Card[]) => {
@@ -31,6 +31,14 @@ export const useGameLogic = (cards: Card[]) => {
     return () => clearInterval(t)
   }, [timerRunning])
 
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+    }
+  }, [])
+
   const handleCardClick = useCallback(
     (cardId: Card['id']) => {
       if (disabled || flipped.includes(cardId) || matched.includes(cardId)) {
@@ -53,7 +61,7 @@ export const useGameLogic = (cards: Card[]) => {
           setFlipped([])
           setDisabled(false)
         } else {
-          setTimeout(() => {
+          timeoutRef.current = setTimeout(() => {
             setFlipped([])
             setDisabled(false)
           }, 1000)
