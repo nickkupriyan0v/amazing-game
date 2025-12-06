759743360318d88a3e310fb06b5a72b85c56a9ee
Aoi <naztya2001@bk.ru>
2025-12-06 19:32:25 +0300

    fix: removed console logs

--- packages/client/src/api/userRequests.ts
+++ packages/client/src/api/userRequests.ts
@@ -27,7 +27,6 @@ export type Response = SuccessResponse | ErrorResponse
 
 export class UserRequestsClass {
   public getUserInfo(): Promise<Response> {
-    console.log('in get user')
     return axios
       .get(urlAPI + '/auth/user', { withCredentials: true })
       .then(response => {
