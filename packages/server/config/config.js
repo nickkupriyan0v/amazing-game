11066e7719c9f22e1709e5714b9fbaffad37cf32
Nurtazenov <a.nurtazenov@yandex.ru>
2025-12-24 01:53:32 +0300

    db

--- packages/server/config/config.js
+++ packages/server/config/config.js
@@ -1,23 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
+require('dotenv').config();
 
-export const development = {
-  username: process.env.POSTGRES_USER || 'myuser',
-  password: process.env.POSTGRES_PASSWORD || 'changeme123',
-  database: process.env.POSTGRES_DB || 'myapp',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_PORT || 5432,
-  dialect: 'postgres',
-}
-export const test = {
-  username: process.env.POSTGRES_USER || 'myuser',
-  password: process.env.POSTGRES_PASSWORD || 'changeme123',
-  database: process.env.POSTGRES_DB || 'myapp',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_PORT || 5432,
-  dialect: 'postgres',
-}
-export const production = {
-  use_env_variable: 'DATABASE_URL',
-  dialect: 'postgres',
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER || 'myuser',
+    password: process.env.POSTGRES_PASSWORD || 'changeme123',
+    database: process.env.POSTGRES_DB || 'myapp',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.POSTGRES_USER || 'myuser',
+    password: process.env.POSTGRES_PASSWORD || 'changeme123',
+    database: process.env.POSTGRES_DB || 'myapp',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
 }
